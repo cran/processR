608f0a1024d06314ab6b92b11fad8620a57cd3eb
Keon-Woong Moon <cardiomoon@gmail.com>
2019-03-07 16:40:03 +0000

    version 0.1.0

---
+++ R/makeEquation.R
@@ -0,0 +1,296 @@
+#'Add line feed to string
+#'
+#'@param x A string
+#'@param ... one or more R objects, to be converted to character vectors.
+addLine=function(x,...){
+    if(x=="") x<-paste0(...)
+    else x<-paste0(x,"\n",...)
+    x
+}
+
+#'Add `+` mark to string
+#'
+#'@param x A string
+#'@param ... one or more R objects, to be converted to character vectors.
+addPlus=function(x,...){
+  if(x=="") x<-paste0(...)
+  else x<-paste0(x,"+",...)
+  x
+}
+
+#' Make mediation equations 1
+#'
+#' @param X A character vectors indicating independent variables
+#' @param M A character vectors indicating mediators
+#' @param stage An integer indicating the order
+#' @param start An integer
+#' @param add2ndMediation whether or not make a secondmediation equation
+makeEquation1=function(X,M,stage=1,start=0,add2ndMediation=TRUE){
+  countX=length(X)
+  countM=length(M)
+
+  equation=""
+
+  for(i in 1:countM){
+    sub=""
+    for(j in 1:countX){
+      sub=addPlus(sub,letters[stage],start+(i-1)*countX+j,"*",X[j])
+    }
+    if(add2ndMediation &(countM>1)&(i>1)){
+      sub=addPlus(sub,"d",i-1,"*",M[i-1])
+    }
+    temp=paste0(M[i],"~",sub)
+    equation=addLine(equation,temp)
+  }
+  equation
+}
+
+#' Make mediation equations 2
+#'
+#' @param X A character vectors indicating independent variables
+#' @param M A character vectors indicating mediators
+#' @param Y A character vectors indicating dependent variables
+makeEquation2=function(X,M,Y){
+  countX=length(X)
+  countM=length(M)
+  countY=length(Y)
+
+  equation=""
+
+  for(i in 1:countY){
+
+    temp=makeEquation1(M,Y[i],stage=2,start=(i-1)*countM)
+    temp
+    sub=""
+    for(j in 1:countX){
+      sub=addPlus(sub,letters[3],(i-1)*countX+j,"*",X[j])
+    }
+    temp=addPlus(temp,sub)
+    equation=addLine(equation,temp)
+  }
+  equation
+}
+
+#' Make mediation equations 3
+#'
+#' @param X A character vectors indicating independent variables
+#' @param M A character vectors indicating mediators
+#' @param Y A character vectors indicating dependent variables
+#' @param add2ndMediation whether or not make a secondmediation equation
+makeEquation3=function(X,M,Y,add2ndMediation=TRUE){
+  (countX=length(X))
+  (countM=length(M))
+  (countY=length(Y))
+
+  equation=""
+  ind=c()
+  for(k in 1:countY){
+    for(i in 1:countX) {
+      for(j in 1:countM) {
+
+        noA=(i-1)*countM+j
+        no=noA+(k-1)*countX*countM
+        b=((noA-1)%/%countX)+1+(k-1)*countM
+       #temp=paste0("ind",no,":=a",noA,"*b",b)
+       #equation=addLine(equation,temp)
+        ind=c(ind,paste0("a",noA,"*b",b))
+        temp=paste0("ind",length(ind),":=",ind[length(ind)])
+        equation=addLine(equation,temp)
+      }
+    }
+  }
+  equation
+  secondInd=c()
+  if(add2ndMediation &(countM>1)){
+    for(k in 1:countY){
+      for(j in 2:countM){
+        equationa=""
+        for(i in 1:countX){
+          start=(j-2)*countX
+          tempa=paste0("a",start+i,"*d",j-1,"*b",j+(k-1)*countM)
+          equationa=addPlus(equationa,tempa)
+        }
+        #temp=paste0("secondInd",(j-1)+(k-1)*(countM-1),":=",equationa)
+        secondInd=c(secondInd,equationa)
+        temp=paste0("secondInd",length(secondInd),":=",secondInd[length(secondInd)])
+        equation=addLine(equation,temp)
+      }
+    }
+  }
+  equation
+  thirdInd=c()
+  if(add2ndMediation &(countM>2)){
+    for(k in 1:countY){
+      for(j in 3:countM){
+        equationa=""
+        for(i in 1:countX){
+          start=(j-3)*countX
+          tempa=paste0("a",start+i,"*d",j-2,"*d",j-1,"*b",j+(k-1)*countM)
+          equationa=addPlus(equationa,tempa)
+        }
+        #temp=paste0("thirdInd",j-2,":=",equationa)
+        #temp=paste0("thirdInd",(j-2)+(k-1)*(countM-2),":=",equationa)
+        thirdInd=c(thirdInd,equationa)
+        temp=paste0("thirdInd",length(thirdInd),":=",thirdInd[length(thirdInd)])
+        equation=addLine(equation,temp)
+      }
+    }
+  }
+  equation
+  ## total effect
+  total=c()
+
+  for(k in 1:countY){
+       # direct effect
+       direct=paste0("c",seq(1:countX)+(k-1)*countX)
+       Effect=Reduce(addPlus,direct)
+       # indirect effect
+      if(countM>=1){
+       start=1+(k-1)*length(ind)/countY
+       end=start+length(ind)/countY-1
+       indirectEffect=Reduce(addPlus,ind[start:end])
+       Effect=addPlus(Effect,indirectEffect)
+      }
+       # secondIndirect
+       if(countM>=2){
+       start=1+(k-1)*length(secondInd)/countY
+       end=start+length(secondInd)/countY-1
+       secondIndEffect=Reduce(addPlus,secondInd[start:end])
+       if(add2ndMediation) Effect=addPlus(Effect,secondIndEffect)
+       }
+       # thirdIndirect
+       if(countM>=3){
+       start=1+(k-1)*length(thirdInd)/countY
+       end=start+length(thirdInd)/countY-1
+       thirdIndEffect=Reduce(addPlus,thirdInd[start:end])
+       if(add2ndMediation) Effect=addPlus(Effect,thirdIndEffect)
+
+       }
+       temp=paste0("total",k,":=",Effect)
+       equation=addLine(equation,temp)
+
+  }
+
+
+  equation
+}
+
+
+#' Make mediation equations 3
+#'
+#' @param X A character vectors indicating independent variables
+#' @param M A character vectors indicating mediators
+#' @param Y A character vectors indicating dependent variables
+#' @param add2ndMediation whether or not make a secondmediation equation
+#' @param covar Optional list of covariates
+#' @export
+#' @examples
+#' X="X";M=c("M1","M2","M3");Y=c("Y1","Y2");add2ndMediation=TRUE
+#' covar=list(name=c("C1","C2","C3"),label=c("ese","sex","tenure"),site=list(c("M1","Y1"),"Y2","Y2"))
+#' cat(makeEquation(X,M,Y,covar=covar))
+makeEquation=function(X,M,Y,add2ndMediation=TRUE,covar=list()){
+    # cat("X=",X,"\n")
+    # cat("M=",M,"\n")
+    # cat("Y=",Y,"\n")
+    # cat("str(covar)")
+    # str(covar)
+    (countX=length(X))
+    (countM=length(M))
+    (countY=length(Y))
+    if(countX*countM*countY==0) {
+      equation=" # You need at least one dependent variable(s),\n#one mediation variable(s) and one independent variable(s)."
+    } else{
+      temp=makeEquation2(X,M,Y)
+      temp
+      temp=addCovarEquation(temp,covar,prefix="g")
+      equation=paste0("# Mediation Effect\n",temp)
+      temp=makeEquation1(X,M,add2ndMediation=add2ndMediation)
+      temp=addCovarEquation(temp,covar,prefix="h")
+      equation=addLine(equation,temp)
+      equation=addLine(equation,makeEquation3(X,M,Y,add2ndMediation=add2ndMediation))
+    }
+  equation
+}
+
+#' Add covariates to equation
+#' @param equation The equation
+#' @param covar A list
+#' @param prefix prefix
+#' @param grouplabels A list
+#' @importFrom stringr str_trim
+#' @export
+#' @examples
+#' equation="M ~ X*W\nY ~ a1*M + C1*X"
+#' covar=list(name=c("C1","C2","C3"),label=c("ese","sex","tenure"),site=list(c("M","Y"),"Y","Y"))
+#' grouplabels=list(C1="e")
+#' addCovarEquation(equation,covar=covar)
+addCovarEquation=function(equation,covar=list(),prefix="f",grouplabels=NULL){
+
+
+  temp1=unlist(strsplit(equation,"\n"))
+  temp1
+  temp2=strsplit(temp1,"~")
+  temp2
+  result=list()
+  start=1
+  i=1
+  for(i in 1:length(temp2)){
+      var=temp2[[i]][1]
+      var=str_trim(var,side="both")
+      covar
+      temp3=seekVar(covar=covar,var=var,prefix=prefix,start=start,grouplabels=grouplabels)
+      temp3
+      if(is.null(temp3)){
+          result[[i]]=paste(var,"~",temp2[[i]][2])
+      } else {
+          temp4=paste(temp2[[i]][2],"+",paste(temp3,collapse=" + "))
+          result[[i]]=paste(var,"~",temp4)
+          start=start+length(temp3)
+      }
+  }
+  paste(unlist(result),collapse="\n")
+}
+
+#' Seek var form covariates
+#' @param covar A list of covariates
+#' @param var A name of variable to look for
+#' @param prefix A prefix
+#' @param start A start number
+#' @param grouplabels A list
+#' @export
+#' @examples
+#' covar=list(name=c("C1","C2","C3"),label=c("ese","sex","tenure"),site=list(c("M","Y"),"Y","Y"))
+#' var="M"
+#' seekVar(covar,var,prefix=NULL)
+seekVar=function(covar=list(),var,prefix="h",start=1,grouplabels=NULL){
+  temp=c()
+  if(length(covar$name)>0){
+    j=start
+    for(i in 1:length(covar$name)){
+      temp=c(temp,covar$name[i])
+    }
+    res=c()
+    for(i in seq_along(temp)){
+        if(temp[i] %in% names(grouplabels)){
+          count=attr(grouplabels[[temp[i]]],"length")
+          temp1=paste0(grouplabels[[temp[i]]],1:count)
+          res=c(res,temp1)
+        } else{
+          res=c(res,temp[i])
+        }
+    }
+    temp=c()
+    j=1
+    for(i in seq_along(res)){
+        if(!is.null(prefix)) {
+          temp=c(temp,paste0(prefix,j,"*",res[i]))
+        } else{
+          temp=c(temp,res[i])
+        }
+        j=j+1
+    }
+  }
+  temp
+}
+
+
---
+++ README.md
@@ -0,0 +1,83 @@
+R package processR  <img src="inst/figures/imgfile.png" align="right" height="120" width="103.6"/>
+=========================================================
+
+
+
+The `processR` package aims to be a user-friendly way to perform moderation, mediation, moderated mediation and moderated moderation in R. This package is inspired from famous PROCESS macro for SPSS and SAS created by Andrew Hayes. 
+
+## PROCESS macro and R package `processR`
+
+Andrew F. Hayes was not involved in the development of this R package or application and cannot attest to the quality of the computations implemented in the code you are using. Use at your own risk.
+
+## Installation
+
+You can install the `processR` package from github.
+
+
+```r
+if(!require(devtools)) install.packages("devtools")
+devtools::install_github("cardiomoon/processR")
+```
+ 
+## What does this package cover ? 
+
+The `processR` package covers moderation, mediation, moderated mediation and moderated moderation with R. Supporting models are as follows.
+
+
+```r
+library(processR)
+sort(pmacro$no)
+```
+
+```
+ [1]  0.0  1.0  2.0  3.0  4.0  4.2  5.0  6.0  6.3  6.4  7.0  8.0  9.0 10.0
+[15] 11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0 20.0 21.0 22.0 23.0 24.0
+[29] 28.0 29.0 30.0 31.0 35.0 36.0 40.0 41.0 45.0 49.0 50.0 58.0 59.0 60.0
+[43] 61.0 62.0 63.0 64.0 65.0 66.0 67.0 74.0 75.0 76.0
+```
+
+Currently, 52 models are supported.
+
+## Example: Moderated Mediation (PROCESS macro model 8)
+
+I will explain functions of processR package by a example.
+
+## Concept Diagram and Statistical Diagram
+
+You can draw concept diagram and statistical diagram easily. For example, you can draw the concept diagram for PROCESS macro model 8.
+
+
+```r
+pmacroModel(8)
+```
+
+<img src="https://github.com/cardiomoon/processR/blob/master/figure/fig1.png?raw=true"  style="display: block; margin: auto;" />
+
+
+You can draw statistical diagram of this model.
+
+
+```r
+statisticalDiagram(8)
+```
+
+<img src="https://github.com/cardiomoon/processR/blob/master/figure/fig2.png?raw=true"  style="display: block; margin: auto;" />
+
+
+## Full vignette
+
+You can see full vignette for model 8 at http://rpubs.com/cardiomoon/468602
+
+
+## Shiny App
+
+I have developed a shiny app. You can test the app at http://web-r.space:3838/processR.
+I will appreciate any comment.
+
+## How to perform this analysis with shiny app
+
+You can see how to perform this analysis at http://rpubs.com/cardiomoon/468600
+
+## Sample powerpoint file
+
+In the shiny app, you can download the analysis results as a powerpoint file. You can download the sample file [model8.pptx](https://github.com/cardiomoon/processRDocs/blob/master/model8/model8.pptx?raw=true) - view with [office web viewer](https://view.officeapps.live.com/op/view.aspx?src=https://github.com/cardiomoon/processRDocs/blob/master/model8/model8.pptx?raw=true). 
